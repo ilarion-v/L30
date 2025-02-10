904710778f661ef450306584506b0ba37fab9ada
ilarion-v <ilarionvl@gmail.com>
2025-02-10 16:06:10 -0600

    hw30

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import swapiReducer from "./swapiSlice";
+
+export const store = configureStore({
+  reducer: {
+    swapi: swapiReducer,
+  },
+});
+
+export default store;
